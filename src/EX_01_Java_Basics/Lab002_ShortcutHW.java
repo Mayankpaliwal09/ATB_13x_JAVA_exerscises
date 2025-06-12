0c7ad1d53f1ca8dd7d1947f6a04c8a430ca34bb8
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-06-12 20:02:46 +0530

    updated

---
+++ src/EX_01_Java_Basics/Lab002_ShortcutHW.java
@@ -0,0 +1,12 @@
+package EX_01_Java_Basics;
+
+public class Lab002_ShortcutHW {
+    public static void main(String[] args) {
+        System.out.println("HEllo test ");
+    }
+
+    public static void main(int args) {
+
+    }
+}
+
