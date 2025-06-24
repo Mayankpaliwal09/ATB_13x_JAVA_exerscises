b808fd0c1bf7d369523223a00af49c78a104bd62
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-06-24 22:46:21 +0530

    18th June task

---
+++ src/Tasks_java_ATX13/Tasks_18th_June/Grading.java
@@ -0,0 +1,43 @@
+package Tasks_java_ATX13.Tasks_18th_June;
+
+import java.util.Scanner;
+
+public class Grading {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Marks :");
+        if(sc.hasNextInt()){
+            int marks = sc.nextInt();
+            if(marks >= 0 && marks <= 100){
+                CalculateGrade(marks);
+            } else {
+                System.out.println("please enter marks in between 0-100");
+            }
+        } else {
+            System.out.println("Please enter the valid marks");
+
+        }
+
+
+
+    }
+
+    private static void CalculateGrade(int marks) {
+
+        if(marks >= 90){
+            System.out.println("Grade is  : +A");
+        } else if (marks >= 80 ) {
+            System.out.println("Grade is  : A");
+        } else if (marks >= 70 ) {
+            System.out.println("Grade is  : B");
+        } else if (marks >= 60 ) {
+            System.out.println("Grade is  : C");
+        } else if (marks >= 50 ) {
+            System.out.println("Grade is  : D");
+        } else if (marks >= 40 ) {
+            System.out.println("Grade is  : E");
+        } else {
+            System.out.println("You are fail ");
+        }
+    }
+}
---
+++ src/Tasks_java_ATX13/Tasks_18th_June/Leap_year.java
@@ -0,0 +1,27 @@
+package Tasks_java_ATX13.Tasks_18th_June;
+
+import java.util.Scanner;
+
+public class Leap_year {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the year");
+        int year = sc.nextInt();
+
+        CheckLeapYear(year);
+        sc.close();
+    }
+
+
+    private static void CheckLeapYear(int year) {
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+
+            System.out.println(year + " is a leap year");
+
+        } else {
+            System.out.println(year + " is a Not leap year");
+        }
+    }
+}
