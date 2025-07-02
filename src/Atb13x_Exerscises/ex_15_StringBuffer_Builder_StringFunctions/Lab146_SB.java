feed627e675f24f90e380e2bb8c9146a440e57aa
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-07-02 19:01:11 +0530

    Task 24th june string builder , buffer

---
+++ src/Atb13x_Exerscises/ex_15_StringBuffer_Builder_StringFunctions/Lab146_SB.java
@@ -0,0 +1,10 @@
+package Atb13x_Exerscises.ex_15_StringBuffer_Builder_StringFunctions;
+
+public class Lab146_SB {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Pramod");
+        stringBuilder.append(123);
+
+        System.out.println(stringBuilder);
+    }
+}
