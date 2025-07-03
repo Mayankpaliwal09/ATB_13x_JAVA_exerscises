e5cbf8f81e020570958a5bb99226b84b4e7d9e71
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-07-03 21:28:00 +0530

    30th june oops

---
+++ src/Atb13x_Exerscises/ex_17_OOPs/Dog.java
@@ -0,0 +1,14 @@
+package Atb13x_Exerscises.ex_17_OOPs;
+
+public class Dog {
+    String name;
+    String breed;
+    String color_eyes;
+
+
+    void walk() {
+    }
+
+    void bark() {
+    }
+}
