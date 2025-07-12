608ad94f3295afcd6af00a1c5980aa5d997c21d8
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-07-12 22:11:30 +0530

    updated access public class from diff package

--- src/Tasks_java_ATX13/Tasks_12Th_june_ternary/Even_odd_ternary.java
+++ src/Tasks_java_ATX13/Tasks_12Th_june_ternary/Even_odd_ternary.java
@@ -1,4 +1,6 @@
 package Tasks_java_ATX13.Tasks_12Th_june_ternary;
+import Tasks_java_ATX13.Tasks_4thJuly_AccessModifier.Prog7.*;
+import Tasks_java_ATX13.Tasks_4thJuly_AccessModifier.Stud;
 
 public class Even_odd_ternary {
     public static void main(String[] args) {
@@ -8,5 +10,11 @@ public class Even_odd_ternary {
 
         String result = a%2==0 ? a+ "is even" : a+" is odd";
         System.out.println(result);
+
+        Stud s1 = new Stud();
+        s1.showInfo();
+
+
+
     }
 }
