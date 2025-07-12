45ac277f962e9fc14eb27ab6ec5ab4db1e4455af
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-07-12 22:14:42 +0530

    8 July 2025 Enum

--- src/Atb13x_Exerscises/ex_28_ENUM/Lab211_ENUM.java
+++ src/Atb13x_Exerscises/ex_28_ENUM/Lab211_ENUM.java
@@ -4,6 +4,7 @@ public class Lab211_ENUM {
     public static void main(String[] args) {
 
         System.out.println(Locators.page_input_email.getLocators());
+//        System.out.println(Locators.page_input_email);
         System.out.println(Locators.page_input_password.getLocators());
         System.out.println(Locators.page_button.getLocators());
 
